6e2f116fed4ecc11c454db32a59cbb6d7be08ea7
unknown <mos.gom.17@gmail.com>
2025-04-27 12:11:12 +0300

    Authentication

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
-const taskRouter= require('./routers/task')
+const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3001
 
@@ -12,3 +12,16 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+
+//const bcrypt=require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () => {
+   const token = jwt.sign({ _id:'abc1233'}, 'thisismynewcourse', {expiresIn:'7 days'})
+   const data =jwt.verify(token , 'thisismynewcourse')
+   console.log(token)
+   console.log(data)
+}
+
+myFunction()
--- src/routers/task.js
+++ src/routers/task.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const Task= require('../models/task')
+const Task = require('../models/task')
 const router = new express.Router()
 
 router.post('/tasks', async (req, res) => {
@@ -44,7 +44,10 @@ router.patch('/tasks/:id', async (req, res) => {
         return res.status(400).send({ error: 'Invalid updates!' })
     }
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        const task = await Task.findById(req.params.id)
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
         if (!task) {
             return res.status(404).send()
         }
