70f355ddccd4c0e8e7d46d668ccb9d5297536be9
unknown <mos.gom.17@gmail.com>
2025-05-04 10:48:38 +0300

    Init commit

--- src/models/user.js
+++ src/models/user.js
@@ -84,7 +84,7 @@ userSchema.methods.toJSON = function () {
     delete userObject.password
     delete userObject.tokens
     delete userObject.avatar
-    
+
     return userObject
 }
 
@@ -92,7 +92,7 @@ userSchema.methods.toJSON = function () {
 // Generating Token
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({ _id: user.id.toString() }, 'thisismynewcourse')
+    const token = jwt.sign({ _id: user.id.toString() }, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const User = require('../models/user')
 const sharp = require('sharp')
 const auth = require('../middleware/auth')
 const multer = require('multer')
+const {sendWelcomeEmail,sendCancelationEmail}= require ('../emails/account')
 const router = new express.Router()
 
 
@@ -10,6 +11,7 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
@@ -104,7 +106,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
-
+        sendCancelationEmail(req.user.email,req.user.name)
         res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
